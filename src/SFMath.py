fad2bf07898e008167896b68b010746ba4223655
Franciszek Slusarczyk <frslusarczyk@student.agh.edu.pl>
2025-08-11 15:36:10 +0200

    add logical metohods to SFMath.KinematicVector. small improvments of SFMath.KinematicVector code. add SFMath.CoordinateSystem class

--- src/SFMath.py
+++ src/SFMath.py
@@ -42,22 +42,18 @@ class KinematicVector:
                     raise Exception(f"You can use 0, 1 or 3 arbitrary arguments, but there is {str(len(args))}")
         if kwargs:
             for key, value in kwargs.items():
-                if key == "x":
-                    if type(value) not in TYPES_TO_FLOAT:
-                        raise Exception(f"Argument '{str(value)}' must be one of this types: {str(TYPES_TO_FLOAT)}")
-                    self.x = float(value)
-                    continue
-                if key == "y":
-                    if type(value) not in TYPES_TO_FLOAT:
-                        raise Exception(f"Argument '{str(value)}' must be one of this types: {str(TYPES_TO_FLOAT)}")
-                    self.y = float(value)
-                    continue
-                if key == "z":
-                    if type(value) not in TYPES_TO_FLOAT:
-                        raise Exception(f"Argument '{str(value)}' must be one of this types: {str(TYPES_TO_FLOAT)}")
-                    self.z = float(value)
-                    continue
-                raise Exception(f"Incorrect keyword argument: {str(key)}")
+                if type(value) not in TYPES_TO_FLOAT:
+                    raise Exception(f"Argument '{str(key)}' must be one of this types: {str(TYPES_TO_FLOAT)} not {str(type(value))}")
+                print(key)
+                match key:
+                    case "x":
+                        self.x = float(value)
+                    case "y":
+                        self.y = float(value)
+                    case "z":
+                        self.z = float(value)
+                    case _:
+                        raise Exception(f"Incorrect keyword argument: {str(key)}")
 
     def __repr__(self) -> str:
         """method returning a string representation of the vector
@@ -94,7 +90,7 @@ class KinematicVector:
         :return: vector's length"""
         return self.length()
 
-    def __add__(self, other) -> 'KinematicVector':
+    def __add__(self, other: 'KinematicVector') -> 'KinematicVector':
         """method summing two vectors
         :param other: second vector to add
         :return: sum of the vectors"""
@@ -102,7 +98,7 @@ class KinematicVector:
             raise Exception(f"The {other} argument has an incorrect type {type(other)}")
         return KinematicVector(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self, other) -> 'KinematicVector':
+    def __sub__(self, other: 'KinematicVector') -> 'KinematicVector':
         """method subtracting two vectors
         :param other: second vector to subtract
         :return: difference of the vectors"""
@@ -110,7 +106,7 @@ class KinematicVector:
             raise Exception(f"The {other} argument has an incorrect type {type(other)}")
         return KinematicVector(self.x - other.x, self.y - other.y, self.z - other.z)
 
-    def __mul__(self, other) -> 'KinematicVector':
+    def __mul__(self, other: float | int) -> 'KinematicVector':
         """method multiplying scalar and a vector
         :param other: number to multiply
         :return: vectors multiplied by the scalar"""
@@ -118,7 +114,7 @@ class KinematicVector:
             raise Exception(f"The {other} argument has an incorrect type {type(other)}")
         return KinematicVector(self.x * other, self.y * other, self.z * other)
 
-    def __truediv__(self, other) -> 'KinematicVector':
+    def __truediv__(self, other: float | int) -> 'KinematicVector':
         """method dividing vector by a scalar
         :param other: number to divide
         :return: vectors multiplied by the scalar"""
@@ -128,7 +124,7 @@ class KinematicVector:
             raise Exception(f"Cannot divide by zero")
         return KinematicVector(self.x / other, self.y / other, self.z / other)
 
-    def __matmul__(self, other) -> float:
+    def __matmul__(self, other: 'KinematicVector') -> float:
         """method calculating scalar product of two vectors
         :param other: second vector
         :return: scalar product of vectors"""
@@ -139,8 +135,78 @@ class KinematicVector:
         :return: new vector identical to the original vector"""
         return KinematicVector(self.x, self.y, self.z)
 
+    def __eq__(self, other: 'KinematicVector') -> bool:
+        """method checking if two vectors are equal
+        :return: result of the comparison"""
+        return self.x == other.x and self.y == other.y and self.z == other.z
 
-def vector_projection(u, v) -> (KinematicVector, KinematicVector):
+    def __ne__(self, other: 'KinematicVector') -> bool:
+        """method checking if two vectors are not equal
+        :return: result of the comparison"""
+        return not self == other
+
+    def __nonzero__(self) -> bool:
+        return abs(self) > 0.0
+
+class CoordinateSystem:
+    def __init__(self, *args, **kwargs) -> None:
+        """class' constructor
+        :param *args: arbitrary positional arguments:
+        - 0 parameters - () - create a default coordinate system:
+            vx = (1,0,0), vy = (0,1,0), vz = (0,0,1)
+        - 1 parameter - (a) - create a scaled default coordinate system:
+            vx = (a,0,0), vy = (0,a,0), vz = (0,0,a)
+        - 3 parameters - (a,b,c) - create a custom coordinate system:
+            vx = a, vy = b, vz = c
+        :param **kwargs: arbitrary keyword arguments:
+        - vx = a - vector vx = a,
+        - vy = b - vector vx = b,
+        - vz = c - vector vx = c,
+        :return: None"""
+        self.vx = KinematicVector(1, 0, 0)
+        self.vy = KinematicVector(0, 1, 0)
+        self.vz = KinematicVector(0, 0, 1)
+        if args:
+            match len(args):
+                case 1:
+                    if type(args[0]) not in TYPES_TO_FLOAT:
+                        raise Exception(f"Argument '{str(args[0])}' must be one of this types: {str(TYPES_TO_FLOAT)}")
+                    self.vx = KinematicVector(1, 0, 0) * args[0]
+                    self.vy = KinematicVector(0, 1, 0) * args[0]
+                    self.vz = KinematicVector(0, 0, 1) * args[0]
+                case 3:
+                    if type(args[0]) != KinematicVector:
+                        raise Exception(f"Argument '{str(args[0])}' must be type: {KinematicVector}")
+                    if type(args[1]) != KinematicVector:
+                        raise Exception(f"Argument '{str(args[1])}' must be type: {KinematicVector}")
+                    if type(args[2]) != KinematicVector:
+                        raise Exception(f"Argument '{str(args[2])}' must be type: {KinematicVector}")
+                    self.vx = args[0].copy()
+                    self.vy = args[1].copy()
+                    self.vz = args[2].copy()
+                case _:
+                    raise Exception(f"You can use 0, 1 or 3 arbitrary arguments, but there is {str(len(args))}")
+        if kwargs:
+            for key, value in kwargs.items():
+                if type(value) != KinematicVector:
+                    raise Exception(f"Argument '{str(value)}' must be type: {KinematicVector} not {str(type(value))}")
+                match key:
+                    case "vx":
+                        self.vx = value.copy()
+                    case "vy":
+                        self.vy = value.copy()
+                    case "vz":
+                        self.vz = value.copy()
+                raise Exception(f"Incorrect keyword argument: {str(key)}")
+
+    def __repr__(self):
+        """method returning a string representation of the coordinate system
+        :return: a string representation of the coordinate system"""
+        return f"[{self.vx},\n {self.vy},\n {self.vz}]"
+
+
+
+def vector_projection_1d(u: KinematicVector, v: KinematicVector) -> (KinematicVector, KinematicVector):
     """function calculating the projection of a vector u on the vector v
     :return: parallel and orthogonal (to the vector v) compound of the vector u"""
     alpha = (u @ v) / (v @ v)
